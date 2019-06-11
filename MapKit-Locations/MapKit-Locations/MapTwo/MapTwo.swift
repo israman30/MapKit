c780d7e4becd2a51f9c4ca6e28f8272c2336a738
Israel Manzo <israelmanzo814@gmail.com>
2019-06-11 17:53:57 -0400

    Added location coordinate with marker

--- MapKit-Locations/MapKit-Locations/MapTwo/MapTwo.swift
+++ MapKit-Locations/MapKit-Locations/MapTwo/MapTwo.swift
@@ -15,5 +15,47 @@ class MapTwo: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(location: initialLocation)
+        
+        // show artwork on map
+        let artwork = Artwork(title: "King David Kalakaua",
+                              locationName: "Waikiki Gateway Park",
+                              discipline: "Sculpture",
+                              coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        mapView.addAnnotation(artwork)
+    }
+    
+    // MARK: - Region radius
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius
+        )
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+// MARK: - Anotation Artwork class
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
     }
 }
