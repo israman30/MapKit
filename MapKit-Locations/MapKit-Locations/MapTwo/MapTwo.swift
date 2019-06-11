40c2b9c4eb776b6bb746f711fbee40cd5ccc49bb
Israel Manzo <israelmanzo814@gmail.com>
2019-06-11 17:44:36 -0400

    Added a new storyboard for new mapkit

---
+++ MapKit-Locations/MapKit-Locations/MapTwo/MapTwo.swift
@@ -0,0 +1,19 @@
+//
+//  MapTwo.swift
+//  MapKit-Locations
+//
+//  Created by Israel Manzo on 6/11/19.
+//  Copyright © 2019 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapTwo: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
