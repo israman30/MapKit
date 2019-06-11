f88e2f38fe848626ed20168f9be3b0ae018312bb
Israel Manzo <israelmanzo814@gmail.com>
2019-06-10 22:12:40 -0400

    Added mapkit and cllocation

--- MapKit-Locations/MapKit-Locations/ViewController.swift
+++ MapKit-Locations/MapKit-Locations/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
+/*
+ 1. Get permission to show location
+ 2. Show user location on the map
+ 3. Update the user location, when user moves
+ */
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // TODO
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // TODO
+    }
+    
 }
 
