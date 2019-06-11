c03cb16de42e3d284ca0780f78bfc56c5df9ff93
Israel Manzo <israelmanzo814@gmail.com>
2019-06-11 10:48:40 -0400

    Udpted user locations and request directions

--- MapKit-Locations/MapKit-Locations/ViewController.swift
+++ MapKit-Locations/MapKit-Locations/ViewController.swift
@@ -18,13 +18,18 @@ import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pinImage: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
     let regionMeters: Double = 10000
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pinImage.layer.cornerRadius = 20
+        pinImage.clipsToBounds = true
         checkLocationServices()
     }
     
@@ -57,6 +62,7 @@ class ViewController: UIViewController {
             centerViewInUserLocation()
             // This line calls the delegate method "A"
             locationManager.startUpdatingLocation()
+            previousLocation = getCenterLocation(for: mapView)
             break
             // 2. When user do not allow the app to use location
             // show alert on how to turn on permissions
@@ -86,23 +92,83 @@ class ViewController: UIViewController {
             mapView.setRegion(region, animated: true)
         }
     }
+    
+    // Center location helper
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    @IBAction func goButton(_ sender: UIButton) {
+        guard let location = locationManager.location?.coordinate else { return }
+        
+        let request = createDirectionRequest(from: location)
+        let directions = MKDirections(request: request)
+    }
+    
+    // MARK: - create directin request function helper
+    func createDirectionRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request {
+        let destinationCoordinate = getCenterLocation(for: mapView).coordinate
+        let startingLocation = MKPlacemark(coordinate: coordinate)
+        let destination = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startingLocation)
+        request.destination = MKMapItem(placemark: destination)
+        request.transportType = .automobile
+        request.requestsAlternateRoutes = true
+        
+        return request
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
     
     // STEP #4
     // "A"
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
-        mapView.setRegion(region, animated: true)
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
     
     // STEP #5
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuth()
     }
     
+}
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        // center of the map
+        let center = getCenterLocation(for: mapView)
+        // geocoder
+        let geoCoder = CLGeocoder()
+        
+        guard var previousLocation = previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            if let error = error {
+                print("Error", error)
+                return
+            }
+            
+            guard let placemark = placemarks?.first else { return }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streeName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self?.addressLabel.text = "\(streetNumber) \(streeName)"
+            }
+        }
+    }
 }
 
